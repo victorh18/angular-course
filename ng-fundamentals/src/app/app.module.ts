5ae391ed6f8ed63330f84164c2e26b106efc01f7
victorh18 <victorwmh18@gmail.com>
2022-04-03 13:03:03 -0400

    initial commit

---
+++ ng-fundamentals/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import {EventsAppComponent } from './events-app.component';
+
+@NgModule({
+  declarations: [
+    EventsAppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
---
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<h1>Welcome to the events page!</h1>'
+})
+export class EventsAppComponent {
+  title = 'App for tech events';
+}
