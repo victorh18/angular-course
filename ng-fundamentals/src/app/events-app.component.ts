7bf81a1a26d3e3e2f2d4ae9b783e94269a928212
victorh18 <victorwmh18@gmail.com>
2022-04-03 13:37:15 -0400

    adding assets

--- ng-fundamentals/src/app/events-app.component.ts
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h1>Welcome to the events page!</h1>'
+  template: `
+    <h1>Welcome to the events page!</h1>
+    <img src="/assets/images/basic-shield.png" />
+  `
 })
 export class EventsAppComponent {
   title = 'App for tech events';
