80c3f943fb2d705ada9d99211149b70d1401ddcf
victorh18 <victorwmh18@gmail.com>
2022-04-11 18:55:54 -0400

    Separating template from component

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -1,21 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
-    "selector": "events-list",
-    "template": `
-        <div>
-            <h1>Upcoming Angular Events</h1>
-            <hr />
-            <div class="well thumbnail hoverwell">
-                <h2>{{event.name}}</h2>
-                <div><strong>Time: </strong>{{event.time}} &nbsp;</div>
-                <div><strong>Date: </strong>{{event.date}}</div>
-                <div><strong>Price: </strong>\${{event.price}}</div>
-                <div><strong>Location: </strong>{{event.location.place}},&nbsp;{{event.location.city}},&nbsp;{{event.location.country}}</div>
-            </div>
-
-        </div>
-    `
+    selector: "events-list",
+    templateUrl: "./events-list.component.html"
 })
 
 export class EventsListComponent {
