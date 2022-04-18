16fbeb56bc4ba39e4ae8a195f87b7c7f88dd2688
victorh18 <victorwmh18@gmail.com>
2022-04-18 18:47:47 -0400

    Added ngFor

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -7,12 +7,20 @@ import { Component, Input, EventEmitter, Output } from "@angular/core";
         <h2>{{event.name}}</h2>
         <div><strong>Time: </strong>{{event.time}} &nbsp;</div>
         <div><strong>Date: </strong>{{event.date}}</div>
-        <div><strong>Price: </strong>\${{event.price}}</div>
-        <div><strong>Location: </strong>{{event.location.place}},&nbsp;{{event.location.city}},&nbsp;{{event.location.country}}</div>
-        <div><button class="btn btn-primary" (click)="handleClick()">Click Me!</button></div>
-        <div><button class="btn btn-primary" (click)="logTitle()">Log Me!</button></div>
+        <div class="pad-left"><strong>Price: </strong>\${{event.price}}</div>
+        <div><strong>Location: </strong>{{event.location.address}},&nbsp;{{event.location.city}},&nbsp;{{event.location.country}}</div>
+        <div class="button-row">
+            <div class="col-md-3"><button class="btn btn-primary" (click)="handleClick()">Click Me!</button></div>
+            <div class="col-md-3"><button class="btn btn-primary" (click)="logTitle()">Log Me!</button></div>
+        </div>
     </div>
-    `
+    `,
+    styles: [`
+        .button-row {display: flex; justify-content: space-between; align-items: center; margin-top: 10px}
+        .thumbnail { min-height: 280px}
+        .pad-left { margin-bottom: 10px; }
+        .well div { color: #bbb; }
+    `]
 })
 
 export class EventThumbnailComponent {
