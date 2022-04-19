4d2e6a9cc8260ed90662549ebe5e92c93141d2a9
victorh18 <victorwmh18@gmail.com>
2022-04-19 18:30:31 -0400

    Adding class binding

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -5,10 +5,25 @@ import { Component, Input, EventEmitter, Output } from "@angular/core";
     template: `
     <div class="well thumbnail hoverwell">
         <h2>{{event.name}}</h2>
-        <div><strong>Time: </strong>{{event.time}} &nbsp;</div>
-        <div><strong>Date: </strong>{{event.date}}</div>
-        <div class="pad-left"><strong>Price: </strong>\${{event.price}}</div>
-        <div><strong>Location: </strong>{{event.location.address}},&nbsp;{{event.location.city}},&nbsp;{{event.location.country}}</div>
+        <div>
+            <strong>Time: </strong>
+            {{event?.time}} &nbsp;
+            <div [ngSwitch]="event?.time" [ngClass]="earlyTimeStyle()">
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
+        </div>
+        <div><strong>Date: </strong>{{event?.date}}</div>
+        <div class="pad-left"><strong>Price: </strong>\${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <strong>Location: </strong>
+            {{event?.location?.address}},&nbsp;{{event?.location?.city}},&nbsp;{{event?.location?.country}}
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            <strong>Online URL: </strong>
+            {{event?.onlineUrl}}
+        </div>
         <div class="button-row">
             <div class="col-md-3"><button class="btn btn-primary" (click)="handleClick()">Click Me!</button></div>
             <div class="col-md-3"><button class="btn btn-primary" (click)="logTitle()">Log Me!</button></div>
@@ -20,6 +35,8 @@ import { Component, Input, EventEmitter, Output } from "@angular/core";
         .thumbnail { min-height: 280px}
         .pad-left { margin-bottom: 10px; }
         .well div { color: #bbb; }
+        .green { color: #009900 !important; }
+        .bold { font-weight: bold}
     `]
 })
 
@@ -32,7 +49,14 @@ export class EventThumbnailComponent {
     }
 
     logTitle() {
-        console.log(`The ${this.event.name} course was logged.`);
-        
+        console.log(`The ${this.event.name} course was logged.`); 
+    }
+
+    earlyTimeStyle() {
+        let earlyStart = (this.event?.time === '8:00 am')
+        return {
+            green: earlyStart,
+            bold: earlyStart
+        }
     }
 }
