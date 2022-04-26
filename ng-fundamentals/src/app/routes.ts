41e37bed036363d2595c9442e8eaf6e99e25028d
victorh18 <victorwmh18@gmail.com>
2022-04-26 19:52:04 -0400

    [ADD] adding user module

--- ng-fundamentals/src/app/routes.ts
+++ ng-fundamentals/src/app/routes.ts
@@ -11,5 +11,7 @@ export const AppRoutes:Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateComponent'] },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
     { path: '404', component: Error404Component },
-    { path:  '', redirectTo: '/events', pathMatch: "full"}
+    { path:  '', redirectTo: '/events', pathMatch: "full"},
+    { path: 'user', 
+        loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
 ]
